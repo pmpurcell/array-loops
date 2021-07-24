43d37c2ae9f76293c55ec08948ac2df9cb1d9b7f
pmpurcell93 <maddenpurcell@gmail.com>
2021-07-24 11:18:55 -0500

    Added filter

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-console.log("Connected!");
+// console.log("Connected!");
+
+const teamTwo = [{
+    id: 1,
+    name: "Madden",
+    email: "maddenpurcell@gmail.com",
+    favoriteColor: "Hunter Green",
+    unique: "I have a pet gecko named Rhae.",
+    image: "https://avatars.githubusercontent.com/u/86082231?v=4",
+},
+{
+    id: 2,
+    name:"Joe",
+    email: "joseph.gibbs1188@gmail.com",
+    favoriteColor: "Blue",
+    unique: "Got a big ol'burn on the weenus",
+    image: "https://avatars.githubusercontent.com/u/83508920?v=4",
+
+},
+{
+    id: 3,
+    name: "Nathan",
+    email: "divorcedlawyer1988@hotmail.com",
+    favoriteColor: "Red",
+    unique: "I produce and edit videos.",
+    image: "https://avatars.githubusercontent.com/u/86999347?v=4",
+
+},
+{
+    id: 4,
+    name: "Elle",
+    email: "hello@ellelawrence.com",
+    favoriteColor: "Black",
+    unique: "I have a full sleeve of tatoos.",
+    image: "https://avatars.githubusercontent.com/u/86808230?v=4",
+
+}
+]
+
+
+
+const filterFavoriteColorandEmail = (array, color, email) => {
+  return array.filter(taco => taco.favoriteColor === color && taco => taco.email === email)
+};
+
+console.log(filterFavoriteColor(teamTwo, "Hunter Green"));
